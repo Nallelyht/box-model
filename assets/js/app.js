2384ea5a5237ffe3a0087716fa677d75f4041e61
Nallely House <nallelyht@gmail.com>
2017-04-17 12:05:52 -0500

    tratando de agregar galeria de imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,23 @@
 //alert("hola");
 var imagen = document.getElementsByClassName("box-work");
 var longitud = imagen.length;
+var cerrar = document.getElementsByClassName("close");
 console.log(imagen);
 console.log(longitud);
-
+cerrar.addEventListener("click", cerrarImagen);
 for (var i = 0; i < longitud; i++){
 	imagen[i].addEventListener("click", mostrarImagen);
+	imagen[i].id="img"+i;
+	
 }
-function mostrarImagen() {
-	alert ("hola");
+function mostrarImagen(){
+	var modal = document.getElementById("modal");
+	
+	modal.style.display = "block";
+	
+	
+}
+function cerrarImagen(){
+	var modal = document.getElementById("modal");
+	modal.sytle.display = "none";
 }
