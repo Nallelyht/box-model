cef3cd46d43a0ed0211644c4b43ba9cb7199c25a
Nallely House <nallelyht@gmail.com>
2017-04-17 12:38:27 -0500

    tratando de dar funcionalidad a galeria de imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,20 @@
 //alert("hola");
 var imagen = document.getElementsByClassName("box-work");
 var longitud = imagen.length;
-var cerrar = document.getElementsByClassName("close");
-console.log(imagen);
-console.log(longitud);
-cerrar.addEventListener("click", cerrarImagen);
+var cerrar = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("modal");
+var imagenModal = document.getElementById("img");
+//console.log(imagen);
+//console.log(longitud);
+
 for (var i = 0; i < longitud; i++){
 	imagen[i].addEventListener("click", mostrarImagen);
 	imagen[i].id="img"+i;
 	
 }
 function mostrarImagen(){
-	var modal = document.getElementById("modal");
-	
 	modal.style.display = "block";
+	imagenModal.src =this.src;
 	
 	
 }
